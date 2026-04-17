4e240e9ecb53a609300cff01a3fd3c1196b52113
BertaCastillo-hub <bertacastillocrespo@gmail.com>
2026-04-17 17:37:18 +0200

    limpieza de ficheros no utilizados

--- Frontend_web_Codigo_Secreto/src/app/routes.js
+++ Frontend_web_Codigo_Secreto/src/app/routes.js
@@ -5,12 +5,10 @@ import { Pantalla00Carga } from "./pantallas/Pantalla00Carga";
 import { Pantalla01Login } from "./pantallas/Pantalla01Login";
 import { Pantalla02Home } from "./pantallas/Pantalla02Home";
 import { Pantalla03MisionesPublicas } from "./pantallas/Pantalla03MisionesPublicas";
-import { Pantalla04PartidaAgente } from "./pantallas/Pantalla04PartidaAgente";
 import { Pantalla05NombreUsuarioNuevo } from "./pantallas/Pantalla05NombreUsuarioNuevo";
 import { Pantalla06Manual } from "./pantallas/Pantalla06Manual";
 import { Pantalla07Lobby } from "./pantallas/Pantalla07Lobby";
 import { Pantalla08Social } from "./pantallas/Pantalla08Social";
-import { Pantalla09PartidaJefe } from "./pantallas/Pantalla09PartidaJefe";
 import { Pantalla10Logros } from "./pantallas/Pantalla10Logros";
 import { Pantalla11Perfil } from "./pantallas/Pantalla11Perfil";
 import { Pantalla12CrearPartida } from "./pantallas/Pantalla12CrearPartida";
@@ -28,11 +26,9 @@ export const router = createBrowserRouter([
       { path: "login", Component: Pantalla01Login },
       { path: "home", Component: Pantalla02Home },
       { path: "misiones-publicas", Component: Pantalla03MisionesPublicas },
-      { path: "partida-agente", Component: Pantalla04PartidaAgente },
       { path: "manual", Component: Pantalla06Manual },
       { path: "lobby/:id_partida", Component: Pantalla07Lobby },
       { path: "social", Component: Pantalla08Social },
-      { path: "partida-jefe", Component: Pantalla09PartidaJefe },
       { path: "nombre-usuario-nuevo", Component: Pantalla05NombreUsuarioNuevo },
       { path: "logros", Component: Pantalla10Logros},
       { path: "perfil", Component: Pantalla11Perfil },
