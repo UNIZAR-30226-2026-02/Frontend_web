47e61444aa95853c76889123022c39cb82d6b609
adriana-sainz <sainzsampedroadriana@gmail.com>
2026-04-23 00:03:44 +0200

    exportar función

--- Frontend_web_Codigo_Secreto/src/app/components/funciones.jsx
+++ Frontend_web_Codigo_Secreto/src/app/components/funciones.jsx
@@ -1,4 +1,4 @@
-const limpiarNombreTema = (nombre) => {
+export const limpiarNombreTema = (nombre) => {
   if (!nombre) return '';
   let base = nombre.split('_')[0]; // 'gold', 'sag', etc.
   return base.charAt(0).toUpperCase() + base.slice(1); // 'Gold', 'Terracotta', etc.
