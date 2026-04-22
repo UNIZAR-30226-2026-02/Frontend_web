1eab497705f3861211d94e5b14bb13acbe7acdaa
adriana-sainz <sainzsampedroadriana@gmail.com>
2026-04-23 00:01:53 +0200

    funciones

--- Frontend_web_Codigo_Secreto/src/app/components/funciones.jsx
+++ Frontend_web_Codigo_Secreto/src/app/components/funciones.jsx
@@ -1,6 +1,5 @@
 const limpiarNombreTema = (nombre) => {
   if (!nombre) return '';
   let base = nombre.split('_')[0]; // 'gold', 'sag', etc.
-  if (base === 'sag') return 'SAGE';
   return base.charAt(0).toUpperCase() + base.slice(1); // 'Gold', 'Terracotta', etc.
 };
