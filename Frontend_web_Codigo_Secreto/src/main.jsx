d2a2dcc554867a401ec18f89a5e96d2e7fb2466e
adriana-sainz <sainzsampedroadriana@gmail.com>
2026-05-12 10:42:27 +0200

    fix imports

--- Frontend_web_Codigo_Secreto/src/main.jsx
+++ Frontend_web_Codigo_Secreto/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './app/App.jsx'
-import { ToastProvider } from './app/components/ToastContext'
+import { ToastProvider } from './app/context/ToastContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
